eb5457e64fa19c205c0f6969fca1b11b53764245
Marcel Mueller <com2040@gmail.com>
2014-09-03 17:57:35 +0200

    Added ui-router dependency

--- app/public/app/src/main.js
+++ app/public/app/src/main.js
@@ -1,2 +1,8 @@
-angular.module('ponies.rocks', []);
+angular.module('ponies.rocks', ["ui.router"])
+.config(["$stateProvider", function($stateProvider) {
+    $stateProvider.state('root', {
+        url: '/',
+        templateUrl: 'main.html'
+    });
+});
 
